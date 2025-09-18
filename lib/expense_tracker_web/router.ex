0e00f7175f303fb32d46dadd58e621252a1badb8
Rowa <jrowah@gmail.com>
2025-09-18 13:32:36 +0300

    generate auth layer

--- lib/expense_tracker_web/router.ex
+++ lib/expense_tracker_web/router.ex
@@ -1,6 +1,8 @@
 defmodule ExpenseTrackerWeb.Router do
   use ExpenseTrackerWeb, :router
 
+  import ExpenseTrackerWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule ExpenseTrackerWeb.Router do
     plug :put_root_layout, html: {ExpenseTrackerWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_user
   end
 
   pipeline :api do
@@ -54,6 +57,44 @@ defmodule ExpenseTrackerWeb.Router do
 
       live_dashboard "/dashboard", metrics: ExpenseTrackerWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+
+  ## Authentication routes
+
+  scope "/", ExpenseTrackerWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    live_session :redirect_if_user_is_authenticated,
+      on_mount: [{ExpenseTrackerWeb.UserAuth, :redirect_if_user_is_authenticated}] do
+      live "/register", UserRegistrationLive, :new
+      live "/login", UserLoginLive, :new
+      live "/reset_password", UserForgotPasswordLive, :new
+      live "/reset_password/:token", UserResetPasswordLive, :edit
+    end
+
+    post "/login", UserSessionController, :create
+  end
+
+  scope "/", ExpenseTrackerWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{ExpenseTrackerWeb.UserAuth, :ensure_authenticated}] do
+      live "/settings", UserSettingsLive, :edit
+      live "/settings/confirm_email/:token", UserSettingsLive, :confirm_email
+    end
+  end
+
+  scope "/", ExpenseTrackerWeb do
+    pipe_through [:browser]
+
+    delete "/logout", UserSessionController, :delete
+
+    live_session :current_user,
+      on_mount: [{ExpenseTrackerWeb.UserAuth, :mount_current_user}] do
+      live "/confirm/:token", UserConfirmationLive, :edit
+      live "/confirm", UserConfirmationInstructionsLive, :new
     end
   end
 end
